4ed10e82d83d513fda8ca1176f6e2c3db95533ea
EC2 Default User <ec2-user@ip-172-31-31-144.ap-northeast-1.compute.internal>
2024-09-10 10:59:18 +0000

    19章

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'homes/about' => 'homes#about', as: 'about'
   root to: "homes#top"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+    
   resources :users, only: [:show, :edit,:update]
 end
